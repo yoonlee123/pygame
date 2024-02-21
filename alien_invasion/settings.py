b0de3822fb282581e6b9822717326e3c633d27ee
yoonlee123 <yoonlee123@outlook.com>
2024-02-22 00:41:45 +1100

    Add game modules for alien invasion game

---
+++ alien_invasion/settings.py
@@ -0,0 +1,36 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+    def __init__(self):
+        """Initialize the game settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (230, 230, 230)
+        # Ship settings
+        self.ship_limit = 3
+        # Bullet settings
+        self.bullet_width = 5
+        self.bullet_height = 15
+        self.bullet_colour = (60, 60, 60)
+        self.bullets_allowed = 10
+        #Alien settings
+        self.fleet_drop_speed = 10
+        
+        self.speedup_scale = 1.5 # How quickly the game speeds up
+        self.score_scale = 2.0 # How quickly the point values increase
+        self.initialize_dynamic_settings()
+    
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+        self.fleet_direction = 1  # 1 represent right -1 represent left
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
